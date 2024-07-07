3383f8e4d95eea03590b9b86b736606571252fe3
HariniChivukula <harini.chivukula94@gmail.com>
2024-07-07 19:25:44 +0530

    Add files via upload

---
+++ day_1.py
@@ -0,0 +1,4 @@
+print("Welcome to the Band Name Generator")
+place=input('Which city did you grow up in?\n')
+pet=input('what is your favourite animal? Enter the plural form\n')
+print('Your band name could be ' + place + " " + pet)
